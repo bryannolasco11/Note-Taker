41983376f5fa5afde2c8736009b77dbaaa5a7331
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-06 15:25:34 -0500

    got hello! to print in the browser.  Forgot to make feature branch.  Will do that now.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,7 @@
+// const router = require('express').Router();
+// const res = require('express/lib/response');
+// const { dataNotes } = require ('/db/db');
+
+// router.get('/dataNotes', (req, res) => {
+//     res.send('Hello!');
+// });
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 
 // require dependencies
 const express = require('express');
+const { notes } = require('./db/db.json');
 const fs = require('fs')
+
+// require route
+//const apiRoutes = require('./routes/apiRoutes');
+//const htmlRoutes = require('./routes/htmlRoutes');
 
 // creates an express server
 const app = express();
 
-// require route
-require('./')
+// creates a route for front end request
+app.get('/api/notes', (req, res) => {
+     let results = notes;
+     console.log(req.query)
+     res.json(results);
+});
 
 
 
 
-
-
-
+      
 
 
 app.listen(3001, () => {
