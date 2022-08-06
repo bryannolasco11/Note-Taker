c03248df4e3f629b09351cdee9a86864687c6742
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-06 12:36:16 -0500

    setting up but need to make a feature branch

---
+++ server.js
@@ -0,0 +1,22 @@
+
+// require dependencies
+const express = require('express');
+const fs = require('fs')
+
+// creates an express server
+const app = express();
+
+// require route
+require('./')
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
